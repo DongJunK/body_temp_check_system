cfc994b48f45942b42e84ce722de61f62b308f39
김동준 <skdltm357@naver.com>
2020-06-06 15:09:23 +0900

    app 구현

--- sensor/app.py
+++ sensor/app.py
@@ -1,6 +1,9 @@
 import json
 from qr_reader import QrCodeReader as QrReader
 from iot_communication import SendLog
+from thermometer import TempControl
+from control_led import LedControl 
+from control_buzzer import BuzzerControl
 
 def init():
     global rasp_id
@@ -9,13 +12,38 @@ def init():
     rasp_id = config["rasp_id"]
 
 def run():
-	#qrReader = QrReader()
-	#tmp = qrReader.getQrCode()
-	#print(tmp)
+	qrReader = QrReader()
 	sendLog = SendLog()
+	tempControl = TempControl()
+	ledControl = LedControl()
+	buzzerControl = BuzzerControl()
+	
 	sendLog.send_log(rasp_id,"5293291","35.7")
-                
+	
+	while True:
+		send_temp = tempControl.get_temp()
+		
+		if sned_temp < 30:
+			continue
 
+		if send_temp < 37.5:
+			ledControl.turn_on_green()
+		else:
+			ledControl.turn_on_red()
+		
+		send_person_id = qrReader.getQrCode()
+		
+		if send_person_id == "False":
+			ledControl.turn_off_green()
+			ledControl.turn_off_red()
+			
+			continue
+
+        buzzerControl.turn_on_buzzer()
+		sendLog.send_log(rasp_id, send_person_id, send_temp)
+	
+	del ledControl
+	del buzzerControl
 
 if __name__ == '__main__':
     init()
---
+++ sensor/thermometer.py
@@ -0,0 +1,3 @@
+
+class TempControl:
+	def get_temp(self):
