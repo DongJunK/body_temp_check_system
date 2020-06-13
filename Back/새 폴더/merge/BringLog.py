af5a44f100e11f542f05619b7ffe17f05559dfd7
monocsp <55018831+monocsp@users.noreply.github.com>
2020-06-13 14:44:11 +0900

    20200613master

--- Back/새 폴더/merge/BringLog.py
+++ Back/새 폴더/merge/BringLog.py
@@ -1,7 +1,11 @@
 import requests
 import datetime
 import time
+from re import search
+from get_token_test import GetToken as Token
+
 token = 'eyJhbGciOiJSUzI1NiJ9.eyJzdmNfdGd0X3NlcSI6IjEwMDAwMDg2NjkiLCJ1c2VyX25hbWUiOiJza2RsdG0zNTciLCJwdWJfdGltZSI6MTU5MTM2NTYzOTIzMSwibWJyX2lkIjoic2tkbHRtMzU3IiwibWJyX3NlcSI6IjEwMDAwMDg1MzQiLCJtYnJfY2xhcyI6IjAwMDMiLCJhdXRob3JpdGllcyI6WyJST0xFX09QRU5BUEkiLCJST0xFX1VTRVIiXSwicGxhdGZvcm0iOiIzTVAiLCJ0aGVtZV9jZCI6IlBUTCIsImNsaWVudF9pZCI6Ik1qWmlaV014T0dOaU5qZzBOR1UyWldKbFl6WXhZbVZoTlRabE9XSXhaREV4TkRNeU1qQTJPVGc0T1RVMCIsImF1ZCI6WyJJT1QtQVBJIl0sInVuaXRfc3ZjX2NkIjoiMDAxIiwic2NvcGUiOlsidHJ1c3QiXSwiZHN0cl9jZCI6IjAwMSIsImNvbXBhbnkiOiJLdCIsIm1icl9ubSI6Iuq5gOuPmeykgCIsImV4cCI6MTU5MTk2NTYzOSwianRpIjoiNWVhNzM2YzktZDcxOS00OTMzLTljYzAtMTg3NGY2OGU5OTQwIn0.EGURrbNdR_myiEs3x-CkPM5QW00o0mKehp6fqUsrcW-Tz9Y7d8ZNgupBF8aODbAZGdaNEGQnz6ugheiO4Ia0Vvb0Mm4eMfxvhPNY0m0SlZ92FMTEFNbONk8TAFKjfcsC5SE46P6VMC9IkUZ9M3KkNwenxeh1FFtMdzlpFkNPLWQ0bSkiH3Ma7M_UZc1qUAVhwqpdbgQgX7KSeu_6U8trTBh2Qldw-FSr1o8OEur3rW_fL9lDSELYhifie47eM5hdrGiHZP7PRuOZ94eSN2A1eKsTHTl0MC3QBC_NiDBaBF_yswIet_sn8YfNW_3Eg1fP3DAPJv0A_qL_ilI-TJ8vkA'
+token = 'eyJhbGciOiJSUzI1NiJ9.eyJzdmNfdGd0X3NlcSI6IjEwMDAwMDg2NjkiLCJ1c2VyX25hbWUiOiJza2RsdG0zNTciLCJwdWJfdGltZSI6MTU5MDY3NjMyNTgyMCwibWJyX2lkIjoic2tkbHRtMzU3IiwibWJyX3NlcSI6IjEwMDAwMDg1MzQiLCJtYnJfY2xhcyI6IjAwMDMiLCJhdXRob3JpdGllcyI6WyJST0xFX09QRU5BUEkiLCJST0xFX1VTRVIiXSwicGxhdGZvcm0iOiIzTVAiLCJ0aGVtZV9jZCI6IlBUTCIsImNsaWVudF9pZCI6Ik1qWmlaV014T0dOaU5qZzBOR1UyWldKbFl6WXhZbVZoTlRabE9XSXhaREV4TkRNeU1qQTJPVGc0T1RVMCIsImF1ZCI6WyJJT1QtQVBJIl0sInVuaXRfc3ZjX2NkIjoiMDAxIiwic2NvcGUiOlsidHJ1c3QiXSwiZHN0cl9jZCI6IjAwMSIsImNvbXBhbnkiOiJLdCIsIm1icl9ubSI6Iuq5gOuPmeykgCIsImV4cCI6MTU5MTI3NjMyNSwianRpIjoiNmQxZTAzYTAtN2I3OS00MWE0LWI0ZGEtNDU0Nzg5ZGI1MDU3In0.MFhpGGMDgNnq3bZ1oPVLoPEjmsfxButL-DzsQLQo0fQVRr1zkcDYA2iaf9kfXyiSZ0tA_uZjVTgIRyRkN6Tvj5uxA1US_SaZILFf1f3d1NbdkLhvdbcRPCn-gRH8jHWCDfn-4OSviJBAkzDR2bQYfzaCElfwpZH_obV0kzmRVlxm2YSY7qgwmzDYpvtVAG6yh-GJdqeRyNN6ULk2Ffs8xmxKwHcalWmhhlKrtdxKHbrEE6mFhkDPdu0qGO36JA_aNrmwOcirLOl-HnB5KKT-eJHWhbEB44OnT8_qrojndbkn8GXicgOTeBY4AsEZBPPKUGJNKoiW4NTKllcpFis7Dw'
 
 
 class GetLog:
@@ -14,7 +18,7 @@ class GetLog:
         else:
             self.datetime_from = int(time.mktime(datetime_from.timetuple())*1000+1000) # +1000 is avoid conflict
 
-    def req(path, method):
+    def req(self, path, method):
         headers = {
             'Authorization': 'Bearer '+token}
         data = {}
@@ -23,17 +27,29 @@ class GetLog:
         url = API_HOST + path
         if method == 'GET':
             return requests.get(url, headers=headers)
+    def getToken(self, token_path):
+        print()
+
 
     def get_log_list(self):
         #resp = GetLog.req('?from=1577804400000&to=1609254000000', 'GET')
         current_timestamp = int(datetime.datetime.now().timestamp()*1000)
         #print(current_timestamp)
         datetime_range = '?from={}&to={}'.format(self.datetime_from, current_timestamp)
-        resp = GetLog.req(datetime_range, 'GET')
+        resp = self.req(datetime_range, 'GET')
         resp_body = resp.json()
         #print(resp_body["data"])
-        #print(resp_body)
-        return resp_body["data"]
+        print(resp_body)
+        try:
+            return resp_body["data"]
+        except KeyError:
+            if search('Access token expired', resp_body['error_description']):
+                #get token test
+                print()
+            else:
+                print(resp_body)
+
+
 
 if __name__ == '__main__':
     a = GetLog(None)
--- Back/새 폴더/merge/get_token_test.py
+++ Back/새 폴더/merge/get_token_test.py
@@ -1,46 +1,49 @@
 import requests
 import base64
 import json
-def getInfo():
-	global iot_info
-	with open('./config/iot_config.json','r') as f:
-		iot_info = json.load(f)
 
-def req():
-	appId = iot_info['appId']
-	secret = iot_info['secret']
-	userId = "'"+iot_info['userId']+"'"
-	userPW = "'"+iot_info['userPW']+"'"
-	data = {
-		'grant_type':'password',
-		'username':userId,
-		'password':userPW
-	}
-	print(appId)
-	print(secret)
-	print(userId)
-	print(userPW)
+class GetToken():
 
-	send = appId + ':' + secret
-	send_encode = send.encode('UTF-8')
-	code = base64.b64encode(send_encode)
-	headers = {
-		'Authorization':'Basic ' + code.decode('utf-8')
-	}
+	def getInfo(self):
+		global iot_info
+		with open('./config/iot_config.json','r') as f:
+			iot_info = json.load(f)
 
-	url = 'https://iotmakers.kt.com/oauth/token'
-	result = requests.post(url, headers=headers, data=data)
-	return result.json()
+	def req(self):
+		appId = iot_info['appId']
+		secret = iot_info['secret']
+		userId = "'"+iot_info['userId']+"'"
+		userPW = "'"+iot_info['userPW']+"'"
+		data = {
+			'grant_type':'password',
+			'username':userId,
+			'password':userPW
+		}
+		print(appId)
+		print(secret)
+		print(userId)
+		print(userPW)
 
-def req_test():
-	token = req()
-	headers = {'Authorization':'Bearer ' + token['access_token']}
-	API_HOST = 'https://iotmakers.kt.com:443/api/v1/streams/iot/log'
+		send = appId + ':' + secret
+		send_encode = send.encode('UTF-8')
+		code = base64.b64encode(send_encode)
+		headers = {
+			'Authorization':'Basic ' + code.decode('utf-8')
+		}
 
-	url = API_HOST + '?from=1577804400000&to=1590678000000'
-	result = requests.get(url, headers=headers)
-	return result.json()
+		url = 'https://iotmakers.kt.com/oauth/token'
+		result = requests.post(url, headers=headers, data=data)
+		return result.json()
 
-getInfo()
-result = req_test()
-print(result)
+	def req_test(self):
+		token = self.req()
+		headers = {'Authorization':'Bearer ' + token['access_token']}
+		API_HOST = 'https://iotmakers.kt.com:443/api/v1/streams/iot/log'
+
+		url = API_HOST + '?from=1577804400000&to=1590678000000'
+		result = requests.get(url, headers=headers)
+		return result.json()
+#
+# getInfo()
+# result = req_test()
+# print(result)
